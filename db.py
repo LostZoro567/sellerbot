b23f8816284b6d80020b9793db8738d6cd7d0f5e
LostZoro567 <149526552+LostZoro567@users.noreply.github.com>
2026-04-09 17:57:01 +0530

    Update db.py

--- db.py
+++ db.py
@@ -6,4 +6,8 @@ load_dotenv()
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
+
+if not url or not key:
+    raise EnvironmentError("SUPABASE_URL and SUPABASE_KEY must be set in .env")
+
 supabase: Client = create_client(url, key)
